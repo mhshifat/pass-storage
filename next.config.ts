91d3239f5abf43a41f9bce0a7db4ca06edf0b16d
mhshifat <mehedihassanshifat01@gmail.com>
2025-04-12 21:42:15 +0600

    ignore ts onuild

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
